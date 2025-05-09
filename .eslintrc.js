91f8e2c667f641888cb15bc56d18d31abccb1e02
ytrsoft <1654486219@qq.com>
2025-05-10 04:30:38 +0800

    husky

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+}
